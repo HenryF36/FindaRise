74acc23f64d687c73c4c20add9539eb6ac9014ec
Henry F <HenryFabinski36@outlook.com>
2024-10-12 11:02:15 -0400

    Added CLOCK

--- FindaRise/MainPage.xaml.cs
+++ FindaRise/MainPage.xaml.cs
@@ -1,14 +1,38 @@
-﻿namespace FindaRise
+﻿using System;
+using System.Timers;
+using Microsoft.Maui.Dispatching;
+
+namespace FindaRise
 {
     public partial class MainPage : ContentPage
     {
-        
+        private static System.Timers.Timer _timer;
 
         public MainPage()
         {
             InitializeComponent();
+            SetTimer();
         }
 
-    }
+        private void SetTimer()
+        {
+            // Create a timer that triggers every second (1000 ms)
+            _timer = new System.Timers.Timer(1000);
+            _timer.Elapsed += OnTimedEvent;
+            _timer.AutoReset = true; // Make sure the timer resets after each interval
+            _timer.Enabled = true;   // Start the timer
+        }
 
+        private void OnTimedEvent(Object source, ElapsedEventArgs e)
+        {
+            DateTime now = DateTime.Now;
+            string formattedTime = now.ToString("hh:mm:ss tt");
+
+            // Update the label on the main thread
+            MainThread.BeginInvokeOnMainThread(() =>
+            {
+                ClockL.Text = formattedTime;  // Safely update the UI
+            });
+        }
+    }
 }
